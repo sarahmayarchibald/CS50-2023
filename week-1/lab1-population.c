b4e5f329b07105e98c4b0be0943d5443456029d1
Sarah-May Archibald <55246965+sarahmayarchibald@users.noreply.github.com>
2023-01-07 20:48:43 +0000

    Create lab1-population.c

---
+++ week-1/lab1-population.c
@@ -0,0 +1,42 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Prompt user for minimum size of the population, loop if number less than 9
+    int min_size;
+    do
+    {
+        min_size = get_int("Enter minimum size of the population: ");
+    }
+    while (min_size < 9);
+
+    // Prompt user for maximum size of the population, loop if number less than minimum size
+    int max_size;
+    do
+    {
+        max_size = get_int("Enter the maximum size of the population? ");
+    }
+    while (max_size < min_size);
+
+    // Calculate number of years until the max population is reached
+    int years = 0;
+    // If starting population same as ending population print 0 years
+    if (min_size == max_size)
+    {
+        printf("Years: %i\n", years);
+        return years;
+
+    }
+    else
+    {
+        do
+        {
+            min_size = min_size + ((min_size / 3) - (min_size / 4));
+            years++;
+        }
+        while (min_size < max_size);
+    }
+    // Print number of years population reached its maximum
+    printf("Years: %i\n", years);
+}
