127026eb7c70188f552097ed1892debbdbd98aa4
Sarah-May Archibald <55246965+sarahmayarchibald@users.noreply.github.com>
2023-01-15 18:53:37 +0000

    Amended for loop for the spaces and #

--- week-1/pset1-mario.c
+++ week-1/pset1-mario.c
@@ -9,22 +9,23 @@ int main(void)
     {
         number = get_int("Enter a number between 1 and 8: ");
 
-    } while (number < 1 || number > 8);
+    } 
+    while (number < 1 || number > 8);
 
-    // Print the height entered by the user
-    printf("Height: %i\n", number);
-
+    //Building the pyramid
     for (int i = 0; i < number; i++)
     {
-        for (int j = 0; j < number - 1; j++)
+        //Printing spaces
+        for (int j = number - i; j > 1; j--)
         {
-            printf(".");
-            
+            printf(" ");
         }
-        printf("#\n");
-        //number = number - 1;
-
+        //Printing "#"
+        for (int k = 0; k <= i; k++)
+        {
+            printf("#");
+        }
+        //Going to the next line
+        printf("\n");
     }
-
-    //printf("\n");
 }
