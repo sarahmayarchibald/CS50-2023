1264548756ede8ce87336fa74a7b444704dfafa1
Sarah-May Archibald <55246965+sarahmayarchibald@users.noreply.github.com>
2023-02-12 09:33:21 +0000

    Create lab2-scrabble.c

---
+++ week-2/lab2-scrabble.c
@@ -0,0 +1,46 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Points assigned to each letter of the alphabet
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int compute_score(string word);
+
+int main(void)
+{
+    // Get input words from both players
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    // Score both words
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+
+    // TODO: Print the winner
+}
+
+int compute_score(string word)
+{
+
+    printf("Words: %s\n", word);
+
+    // TODO: Compute and return score for string
+    for(int i = 0; i < strlen(word); i++)
+    {
+        if (islower(word[i]) || isupper(word[i]))
+        {
+            //printf("ASCII letter: %i\n", word[i]);
+            // Thinks the letter are in order
+            printf("Scrabble point: %i\n", POINTS[i]);
+        }
+        else
+        {
+            return 1;
+        }
+
+
+    }
+    return 0;
+}
