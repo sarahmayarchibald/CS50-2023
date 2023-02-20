82e2bab25d5aa3215b711e2b67d42ab14037bd0a
Sarah-May Archibald <55246965+sarahmayarchibald@users.noreply.github.com>
2023-02-20 09:13:09 +0000

    Update no-vowels.c
    Update switch statement

--- week-2/Practice Problems-Optional/no-vowels.c
+++ week-2/Practice Problems-Optional/no-vowels.c
@@ -11,42 +11,53 @@ string replace(string word);
 
 int main(int argc, string argv[])
 {
+    // Prompt for 1 argument count only
     if (argc <= 1 || argc > 2)
     {
         printf("ERROR: Enter './no-vowels' and a word\n");
         return 1;
     }
-
+    // Assign word prompted to the argc
     string word = argv[1];
-    //printf("User word1: %s\n", word);
 
-    printf("New word: %s\n", replace(word));
+    // Print the changed word
+    printf("%s\n", replace(word));
 }
 
 string replace(string word)
 {
+    // Check the length of the word prompted
     int length = strlen(word);
 
+    // Iterate over each letter to check if a, e, o, i or u
     for (int i = 0; i < length; i++)
     {
-        //printf("User word: %c\n", word[i]);
+        char letter = word[i];
 
-        switch (word[i])
+        // Switch a to 6, e to 3, i to 1, o to 0 and maintain u
+        switch (letter)
         {
-            case 1:
-            if(word[i] == 'a')
-            {
+            case 'a':
                 word[i] = '6';
-                printf("%c", word[i]);
-            }
+                break;
 
-            break;
+            case 'e':
+                word[i] = '3';
+                break;
+
+            case 'i':
+                word[i] = '1';
+                break;
+
+            case 'o':
+                word[i] = '0';
+                break;
 
             default:
-            printf("%c\n", word[i]);
+                letter = word[i];
         }
     }
 
-    //printf("User word: %s\n", word);
+    // Return the new word if any vowels except u
     return word;
 }
