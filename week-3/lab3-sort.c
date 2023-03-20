b4a9bab1b0aadca8087eee8fbbbe3a6ab3ebeab9
Sarah-May Archibald <55246965+sarahmayarchibald@users.noreply.github.com>
2023-03-20 10:17:07 +0000

    Update lab3-sort.c

--- week-3/lab3-sort.c
+++ week-3/lab3-sort.c
@@ -1,2 +1,13 @@
-#include <cs50.h>
-#include <stdio.h>
+sort1 uses: Bubble sort
+
+How do you know?: It is extremely fast in the sorted number series (Omega(n) as best case scenario) but overall slow
+
+sort2 uses: Merge sort
+
+How do you know?: Fastest of all algorithms for all categories of number series. However, it took more time on the sorted 50000.
+Even if the array is perfectly sorted, the algorithm still need to split and recombine.
+
+sort3 uses: Selection sort
+
+How do you know?: Time is between Merge and Bubble sort. Timing are closed for all 50000 numbers which coincides with Tetha(n2). It needs to go through
+all of the elements of an array.
