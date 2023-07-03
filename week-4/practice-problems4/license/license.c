d00fbcfe093cf4b8c5590d216ac21f79c6ed2152
Sarah-May Archibald <55246965+sarahmayarchibald@users.noreply.github.com>
2023-07-03 17:36:29 +0100

    Create license.c

---
+++ week-4/practice-problems4/license/license.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    // Check for command line args
+    if (argc != 2)
+    {
+        printf("Usage: ./read infile\n");
+        return 1;
+    }
+
+    // Create buffer to read into
+    char buffer[7];
+
+    // Create array to store plate numbers
+    char *plates[8];
+
+    FILE *infile = fopen(argv[1], "r");
+
+    int idx = 0;
+
+    while (fread(buffer, 1, 7, infile) == 7)
+    {
+        // Replace '\n' with '\0'
+        buffer[6] = '\0';
+
+        // Save plate number in array with memory allocated
+        plates[idx] = malloc(7);
+
+        // Copy from buffer to plate number array
+        strcpy(plates[idx], buffer);
+
+        idx++;
+    }
+
+
+
+    for (int i = 0; i < 8; i++)
+    {
+        printf("%s\n", plates[i]);
+
+        // Free space
+        free(plates[i]);
+    }
+
+    // Close file
+    fclose(infile);
+
+}
