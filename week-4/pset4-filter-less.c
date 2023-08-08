04db45b6a3834802d30fcc9500cd3a55c2381761
Sarah-May Archibald <55246965+sarahmayarchibald@users.noreply.github.com>
2023-08-08 06:37:33 +0100

    Update pset4-filter-less.c blur and reverse

--- week-4/pset4-filter-less.c
+++ week-4/pset4-filter-less.c
@@ -69,11 +69,79 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE temp[1][1];
+    int pxEnd = width;
+    int pxStart = 0;
+
+    for (int i = 0; i < height; i++)
+    {
+        pxEnd = width - 1;
+        pxStart = 0;
+        int halfWidth = width / 2;
+
+        for (int j = 0; j < halfWidth; j++)
+        {
+            // Using a placeholder
+            temp[0][0] = image[i][pxStart];
+            image[i][pxStart] = image[i][pxEnd];
+            image[i][pxEnd] = temp[0][0];
+
+            pxEnd--;
+            pxStart++;
+        }
+    }
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            float sumBlue = 0;
+            float sumGreen = 0;
+            float sumRed = 0;
+            float counter = 0;
+
+            for (int row = -1; row < 2; row++)
+            {
+                for (int col = -1; col < 2; col++)
+                {
+                    if (i + row < 0 || i + row > height - 1)
+                    {
+                        continue;
+                    }
+
+                    if (j + col < 0 || j + col > width - 1)
+                    {
+                        continue;
+                    }
+
+                    sumBlue += image[i + row][j + col].rgbtBlue;
+                    sumGreen += image[i + row][j + col].rgbtGreen;
+                    sumRed += image[i + row][j + col].rgbtRed;
+                    counter++;
+                }
+            }
+
+            copy[i][j].rgbtBlue = round(sumBlue / counter);
+            copy[i][j].rgbtGreen = round(sumGreen / counter);
+            copy[i][j].rgbtRed = round(sumRed / counter);
+        }
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtBlue = copy[i][j].rgbtBlue;
+            image[i][j].rgbtGreen = copy[i][j].rgbtGreen;
+            image[i][j].rgbtRed = copy[i][j].rgbtRed;
+        }
+
+    }
     return;
 }
